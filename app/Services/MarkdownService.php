136cce9433f2534ab677a1b128a2e207bbf3e807
SiteBot <SiteBot@example.com>
2026-02-20 17:44:22 +0300

    Добавил ещё тегов markdown

--- app/Services/MarkdownService.php
+++ app/Services/MarkdownService.php
@@ -35,6 +35,7 @@ class MarkdownService
         $html = strip_tags($html, [
             'p', 'br', 'strong', 'em', 'code', 'pre', 'del',
             'ul', 'ol', 'li', 'blockquote',
+            'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'hr',
         ]);
 
         return trim($html);
